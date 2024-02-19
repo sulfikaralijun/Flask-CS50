ed83a8c599f0f84c5a1abe222ecaf98a19730cc4
Sulfikar Alijun <sulfikaralijun@gmail.com>
2024-02-19 20:31:54 +0800

    update app.py

--- hello/app.py
+++ hello/app.py
@@ -2,10 +2,8 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# @app.route('/greet')
-# def index():
-#     name = request.args.get('name', 'World')
-#     return render_template('index.html', name=name)
 @app.route('/')
 def indexs():
     return render_template('index.html')
+
+@app.route('/greet')
