f1c2ac5c856e07c8aeb3dbebb010b12f06292a81
Sulfikar Alijun <sulfikaralijun@gmail.com>
2024-02-19 21:04:58 +0800

    update app.cpython-310.pyc, app.py and hello/templates/index.html

--- hello/app.py
+++ hello/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def indexs():
     return render_template('index.html')
 
-@app.route('/greet' , methods=['GET'])
+@app.route('/greet' , methods=['POST'])
 def greet():
     name = request.args.get('name', 'Anonym')
     return render_template('greet.html', name=name)
