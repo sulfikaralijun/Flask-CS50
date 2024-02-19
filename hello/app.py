03d89dac5b53e4d4ff6941a94728dc8b72badd77
Sulfikar Alijun <sulfikaralijun@gmail.com>
2024-02-19 20:33:51 +0800

    update app.cpython-310.pyc and app.py

--- hello/app.py
+++ hello/app.py
@@ -7,3 +7,6 @@ def indexs():
     return render_template('index.html')
 
 @app.route('/greet')
+def greet():
+    name = request.args.get('name', 'Anonym')
+    return render_template('index.html', name=name)
