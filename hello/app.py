4ff41665c12e547a4c94a2198243917933d9348b
Sulfikar Alijun <sulfikaralijun@gmail.com>
2024-02-19 20:30:33 +0800

    update app.cpython-310.pyc, app.py and hello/templates/index.html

--- hello/app.py
+++ hello/app.py
@@ -2,7 +2,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# @app.route('/greet')
+# def index():
+#     name = request.args.get('name', 'World')
+#     return render_template('index.html', name=name)
 @app.route('/')
-def index():
-    name = request.args.get('name', 'World')
-    return render_template('index.html', name=name)
+def indexs():
+    return render_template('index.html')
