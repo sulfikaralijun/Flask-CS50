43a9ebb83faa01eb918e5578ae1aaa1b6fd28df9
Sulfikar Alijun <sulfikaralijun@gmail.com>
2024-02-19 21:14:39 +0800

    update app.py

--- hello/app.py
+++ hello/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def indexs():
+    if request.method == 'POST':
+        name = request.form.get('name', 'Anonym')
     return render_template('index.html')
