8cfa146a6b8f57088225b567065dbba910e278bb
Sulfikar Alijun <sulfikaralijun@gmail.com>
2024-02-20 15:44:32 +0800

    feat: create app.py, froshims/templates/index.html and layout.html

---
+++ froshims/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def indexs():
+    if request.method == 'POST':
+        name = request.form.get('name', 'Anonym')
+        return render_template('greet.html', name=name)
+    return render_template('index.html')
