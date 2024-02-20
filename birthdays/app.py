dda358b99916a1093063ab0115b35585e12d6639
Sulfikar Alijun <sulfikaralijun@gmail.com>
2024-02-20 22:16:14 +0800

    update app.py

--- birthdays/app.py
+++ birthdays/app.py
@@ -27,7 +27,7 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
-        
+        name = request.form.get('name')
 
         return redirect("/")
 
